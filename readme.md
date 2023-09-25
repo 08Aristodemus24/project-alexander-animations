390d56a7658f9c2e8d497dcdc2374322869a7297
08Miguel24 <MichaelAveuc571@gmail.com>
2023-09-25 09:17:32 +0800

    created first animations

--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 # A repository that will store all the animations I will test and use in my portfolio website `project-alexander`
+
+1. `manim -pql <scenes.py> <class name>` will create the animation given a "template" defined as our class
+2. `manim -pqh <scenes.py> <class name>` will create the animation in high quality given the `qh` part in the -pqh flag which means quality will be high of the outputted video
---
+++ scenes.py
@@ -0,0 +1,23 @@
+from manim import *
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        circle.set_stroke(PINK, opacity=1)
+        self.play(Create(circle))  # show the circle on screen
+
+
+class TriangleToSquare(Scene):
+    def construct(self):
+        triangle = Triangle()
+        triangle.set_fill(GREEN, opacity=0.5)
+        triangle.set_stroke(GREEN, opacity=1)
+
+        square = Square()
+        square.set_fill(GREEN, opacity=0.5)
+        square.set_stroke(GREEN, opacity=1)
+
+        self.play(Create(triangle))
+        self.play(Transform(triangle, square))
+        self.play(FadeOut(square))
